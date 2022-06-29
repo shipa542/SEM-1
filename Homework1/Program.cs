309b068826649eb260ef788d0ab6d5aa9b6f3e0b
Shipa542 <shipa542@yandex.ru>
2022-06-29 23:45:20 +0300

    Добавлена задача №8

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -46,11 +46,25 @@
 // -3 -> нет
 // 7 -> нет
 
+// Console.Write("введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num % 2 == 1 || num % 2 == -1){
+// Console.WriteLine("no");
+// }
+// else {
+// Console.WriteLine("yes");
+// }
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
 Console.Write("введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if (num % 2 == 1 || num % 2 == -1){
-Console.WriteLine("no");
-}
-else {
-Console.WriteLine("yes");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+if (i % 2 == 0 ){
+Console.WriteLine(i);
 }
