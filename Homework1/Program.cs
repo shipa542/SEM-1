cffa3a39f9b703bcb6064480481903e61130d47c
Shipa542 <shipa542@yandex.ru>
2022-06-29 22:23:49 +0300

    Добавлена задача №2

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+  Console.Write("введите число1: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("введите число2: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    if (num1 > num2)
+      Console.WriteLine("max = " + num1);
+    else{
+      Console.WriteLine("max = " + num2);
+    }
