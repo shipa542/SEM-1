e43ca01f99dfd966da721e24dae84310fc0fa64b
Shipa542 <shipa542@yandex.ru>
2022-06-30 22:18:12 +0300

    Домашние задание №13

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -5,7 +5,28 @@
 // 918 -> 1
 
 
+// Console.Write("введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// num = num / 10 % 10; 
+// Console.WriteLine(num);
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+
 Console.Write("введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-num = num / 10 % 10; 
-Console.WriteLine(num);
+if (num < 99){
+    Console.WriteLine("Третьей цифры нет");
+} 
+ else
+ {Console.WriteLine(num % 10);
+    
+ }
