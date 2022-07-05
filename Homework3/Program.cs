1382be1ad43e27c2a863086bec89933729d5d0e6
Shipa542 <shipa542@yandex.ru>
2022-07-05 22:13:40 +0300

    Домашнее задание №23

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -66,3 +66,30 @@
 //     int z2 = Convert.ToInt32(Console.ReadLine());
 
 //  Console.WriteLine(SystemCoor(x1, y1, z1, x2, y2, z2));   
+
+
+
+
+// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write(num + " -> ");
+
+//  if (num >= 0)
+// {
+//     for (int i = 1; i <= num; i ++)
+//      Console.Write(i * i * i + " ");
+// }
+//  else
+// {
+//     for (int i = -1; i >= num; i --)    
+//         Console.Write(i * i * i + " ");
+
+// }
