2336fd0dcbc4c0846506a4d02f9c52eb9b5fd0ff
Shipa542 <shipa542@yandex.ru>
2022-07-18 22:45:50 +0300

    Домашние задание №50

--- Homework7/Program.cs
+++ Homework7/Program.cs
@@ -19,31 +19,31 @@
 // Console.Clear();
 // Console.WriteLine($"m = {m}, n = {n}.");
 
-// double[,] array = new double[m, n];
+// int[,] array = new int[m, n];
 
-// CreateArrayDouble(array);
+// CreateArrayint(array);
 
 // WriteArray(array);
 
 // Console.WriteLine();
 
-// void CreateArrayDouble(double[,] array)
+// void CreateArrayint(int[,] array)
 // {
 //   for (int i = 0; i < m; i++)
 //   {
 //     for (int j = 0; j < n; j++)
 //     {
-//       array[i, j] = new Random().NextDouble() * 20 - 10;
+//       array[i, j] = new Random().Nextint() * 20 - 10;
 //     }
 //   }
 // }
 
-// void WriteArray (double[,] array){
+// void WriteArray (int[,] array){
 // for (int i = 0; i < m; i++)
 //   {
 //       for (int j = 0; j < n; j++)
 //       {
-//         double alignNumber = Math.Round(array[i, j], 1);
+//         int alignNumber = Math.Round(array[i, j], 1);
 //         Console.Write(alignNumber + " ");
 //       }
 //       Console.WriteLine();
@@ -51,3 +51,62 @@
 // }
 
 
+
+
+
+
+
+
+
+// Console.Clear();
+
+// int[,] GetArray(int m, int n, int minValue, int maxValue) // создает двумерный массив
+// {
+// int[,] result = new int[m, n];
+// for (int i = 0; i < m; i++)
+// {
+// for (int j = 0; j < n; j++)
+// {
+// result[i, j] = new Random().Next(minValue, maxValue + 1);
+// }
+// }
+// return result;
+
+// }
+// void PrintArray(int[,] inArray) // вывводит двумерный массив
+// {
+// for (int row = 0; row < inArray.GetLength(0); row++)
+// {
+// for (int column = 0; column < inArray.GetLength(1); column++)
+// {
+// Console.Write($"{inArray[row,column]} ");
+// }
+// Console.WriteLine();
+// }
+// }
+
+// Console.Write("Введите количество строк массива: ");
+// int rows=Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+// int columns=Convert.ToInt32(Console.ReadLine());
+
+// int[,] array = GetArray(rows, columns, 0, 10);
+// PrintArray(array);
+
+
+// Console.Write("Введите индекс строки массива: ");
+// int m =Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите индекс столбца массива: ");
+// int n =Convert.ToInt32(Console.ReadLine());
+
+// Console.Write($"{m}-{n}");
+
+// if (m < array.GetLength(0) && n < array.GetLength(1)){
+//     Console.WriteLine (" -> такое число есть ");
+// }
+// else{
+//     Console.WriteLine (" -> такого числа нет ");
+    
+// }
