c5266a7dd66c630e841cad5617a7315bf41fca89
Shipa542 <shipa542@yandex.ru>
2022-07-27 15:47:01 +0300

    Домашние задание №64

---
+++ Homework9/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+
+// Console.Clear();
+// int n = InputNumbers("Введите n: ");
+// int m = InputNumbers("Введите m: ");
+
+// Console.Write("->");
+
+// NewMethod(n, m);
+
+// int InputNumbers(string input)
+// {
+//     Console.Write(input);
+//     int output = Convert.ToInt32(Console.ReadLine());
+//     return output;
+// }
+
+// static void NewMethod(int n, int m)
+// {
+//     if (m > n) ;
+//     for (int i = n; i <= m; i++)
+//     {
+//         Console.Write($" {i}");
+       
+//     }
+// }
+
+// Console.WriteLine();
+
+
+
+
